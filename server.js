e749f58ca52991b24416bd2fa3ea55b92b5d2698
danielsangronis1386 <daniel.sangronis1386@gmail.com>
2025-06-23 01:14:29 -0700

    Create your views folder

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const express = require('express');
 
 const app = express();
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs')
+
 // Home route to test that server is working
 app.get('/test', (req, res) => {
   res.send('Graphic Design Server is Running');
 });
+
+// GET / - landing page
+app.get('/', async (req, res) => {
+  res.render('index.ejs');
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
